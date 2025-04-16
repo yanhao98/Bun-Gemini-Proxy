acea8c2017563810ad734873338fad46a792e93b
严浩 <me@yanhao.ren>
2025-04-16 19:22:44 +0800

    优化 MainPage 组件，添加刷新按钮

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -9,8 +9,15 @@ interface Props {
 export function MainPage({ pendingRequests }: Props) {
   return (
     <BaseLayout>
-      运行时间：{prettyMs(process.uptime() * 1000)} | 待处理请求：
-      {pendingRequests.toString()}
+      {`运行时间：${prettyMs(
+        process.uptime() * 1000,
+      )} | 待处理请求：${pendingRequests.toString()}
+      <button
+        onclick="window.location.reload()"
+        style="padding: 2px 8px;"
+      >
+        刷新
+      </button>`}
     </BaseLayout>
   );
 }
