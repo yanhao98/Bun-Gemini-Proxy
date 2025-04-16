f93fe9eac747aa635d2d40fcc009199df00e1383
严浩 <me@yanhao.ren>
2025-04-16 18:46:48 +0800

    增加对 apple-touch-icon-precomposed.png 的支持

--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -10,6 +10,7 @@ function generateFaviconResponse() {
 export const mainRoutes = new Elysia()
   .get('/favicon.ico', generateFaviconResponse)
   .get('/apple-touch-icon.png', generateFaviconResponse)
+  .get('/apple-touch-icon-precomposed.png', generateFaviconResponse)
   .get('/', function get_main(app) {
     return <MainPage pendingRequests={app.server?.pendingRequests || 0} />;
   });
