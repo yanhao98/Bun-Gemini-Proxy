a4d4ae6dda9cbab4fed973239abd77140463aa0b
严浩 <me@yanhao.ren>
2025-04-23 10:28:29 +0800

    删除不再使用的文件和测试代码 [skip ci]

--- _node-redis-keys_.ts
+++
@@ -1,7 +0,0 @@
-import { keyManager } from './src/config/keys';
-
-// await Bun.sleep(1000);
-await keyManager.ready;
-
-const xGoogApiKey = keyManager.getNextApiKey();
-console.log(`[👤] 分配的API密钥: ${xGoogApiKey}`);
--- _some-Worker_.ts
+++
@@ -1,7 +0,0 @@
-// prevents TS errors
-// declare var globalThis: Worker;
-
-globalThis.onmessage = (event: MessageEvent) => {
-  console.log(`[isMainThread: ${Bun.isMainThread}]`, event.data);
-  postMessage('world');
-};
